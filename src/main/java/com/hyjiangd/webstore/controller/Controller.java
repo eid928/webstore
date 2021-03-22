702adb9efc0426be385d10e05110222cbb1c51f3
HY JIANGD <eid928@gmail.com>
2021-03-22 21:02:36 +0800

    cart demo by session, hyjiang, 20210322

--- src/main/java/com/hyjiangd/webstore/controller/Controller.java
+++ src/main/java/com/hyjiangd/webstore/controller/Controller.java
@@ -1,8 +1,20 @@
 package com.hyjiangd.webstore.controller;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import ch.qos.logback.core.joran.spi.ElementSelector;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -50,6 +62,45 @@ public class Controller {
 		model.addAttribute("loginUsername", loginUsername);
 		
 		return "search";
+	}
+	
+	@SuppressWarnings("unchecked")
+	@GetMapping("/cart")
+	public String addItemToCart(HttpServletRequest request) {
+		
+		String loginUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+		request.setAttribute("loginUsername", loginUsername);
+		
+		HttpSession session = request.getSession();
+		String referer = request.getHeader("Referer");
+		System.out.println("Referer = " + referer);
+		List<Map<String, Integer>> cart;
+		
+		if (session.getAttribute("cart") == null) {
+			cart = new ArrayList<Map<String,Integer>>();
+			session.setAttribute("cart", cart);
+		} else {
+			cart = (List<Map<String, Integer>>) session.getAttribute("cart");
+		}
+		
+		int goodsId = Integer.parseInt(request.getParameter("id"));
+		boolean isAlreadyInCart = false;
+		for (Map<String, Integer> item : cart) {
+			if (item.get("goodsId") == goodsId) {
+				isAlreadyInCart = true;
+				item.put("quantity", item.get("quantity") + 1) ;
+			}
+		}
+		if (!isAlreadyInCart) {
+			Map<String, Integer> item = new HashMap<>();
+			item.put("goodsId", goodsId);
+			item.put("quantity", 1);
+			cart.add(item);
+		}
+		
+		System.out.println("session購物車內有: " + request.getSession().getAttribute("cart"));
+		
+		return "redirect:" + referer;
 	}
 	
 	@GetMapping("/login")
