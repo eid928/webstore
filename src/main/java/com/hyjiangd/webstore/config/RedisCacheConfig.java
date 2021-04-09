cb5ce7250161283c6dfde94b030338c18e73750f
HY JIANGD <eid928@gmail.com>
2021-04-09 18:09:00 +0800

    set the RedisCacheManger to use classloader from current thread, solve the conflct bug between devtool and cache serialization

--- src/main/java/com/hyjiangd/webstore/config/RedisCacheConfig.java
+++ src/main/java/com/hyjiangd/webstore/config/RedisCacheConfig.java
@@ -5,7 +5,9 @@ import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -17,7 +19,14 @@ public class RedisCacheConfig extends CachingConfigurerSupport{
 	@Bean
 	public RedisCacheManager cacheManager(RedisConnectionFactory factory) {
 		
-		return RedisCacheManager.create(factory);
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		// to prevent class casting exception caused by devtools
+		// since devtools would use restartClassLoader for POJO in the project, but the object after deserialized from redis cache would be loaded by base ClassLoader
+		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig(classLoader);
+		RedisCacheWriter cacheWriter = RedisCacheWriter.lockingRedisCacheWriter(factory);
+		RedisCacheManager manager = new RedisCacheManager(cacheWriter, redisCacheConfiguration);
+		
+		return manager;
 	}
 	
 	@Bean
